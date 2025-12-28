742b5dfd220b2fe9bd1c05d3832d8c8cf6aeff09
Aaron Tanguma <the_guma@yahoo.com>
2025-12-28 10:59:55 -0700

    created method to check for palindrome, created unit tests

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("cloud", false)]
+        [InlineData("kayak", true)]
+        [InlineData("aaron", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string word, bool expected)
         {
-
+            //Arrange
+            var ws = new WordSmith();
+            //Act
+            var actual = ws.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
